ccd3236fef12e79c84bbcca593d3a963eec47c14
Arman199116 <arman.ochinyan@gmail.com>
2019-05-03 02:32:42 +0400

    send data

--- pages/chose category/images.js
+++ pages/chose category/images.js
@@ -3,13 +3,13 @@ var book_and_author =
 	[
 
         {"authorname":"Hovhannes Tumanyan"},
-        {"img": "../../media/exemp_books/a1.png"},
-        {"img": "../../media/exemp_books/a2.jpg"},
-        {"img": "../../media/exemp_books/a3.jpeg"},
-        {"img": "../../media/exemp_books/a4.jpeg"},
-        {"img": "../../media/exemp_books/a5.jpeg"},
-        {"img": "../../media/exemp_books/a6.jpeg"},
-        {"img": "../../media/exemp_books/a7.jpeg"},
-        {"img": "../../media/exemp_books/a8.jpeg"}
+        {"img": "../../media/exemp_books/a1.png","bookname":"Giqor"},
+        {"img": "../../media/exemp_books/a2.jpg","bookname":"Qarjakner"},
+        {"img": "../../media/exemp_books/a3.jpeg","bookname":"Anush"},
+        {"img": "../../media/exemp_books/a4.jpeg","bookname":"Tern u tsaran"},
+        {"img": "../../media/exemp_books/a5.jpeg","bookname":"Shunn u katun"},
+        {"img": "../../media/exemp_books/a6.jpeg","bookname":"Shunn u katun"},
+        {"img": "../../media/exemp_books/a7.jpeg","bookname":"Shunn u katun"},
+        {"img": "../../media/exemp_books/a8.jpeg","bookname":"Anban hurin"}
 
     ]
--- pages/chose category/project.js
+++ pages/chose category/project.js
@@ -1,24 +1,26 @@
 
-document.getElementById('authorname').innerHTML = book_and_author[0].authorname;
- 
+document.getElementById('authorname').innerHTML = '<i>'+ book_and_author[0].authorname+'</i>';
+
 var images_array = document.querySelectorAll(".images");
 
 for (var i = 0; i < images_array.length; i++) {
-	images_array[i].innerHTML = ('<img onclick=change_location(this) src = '+book_and_author[i+1].img+' >');
-	
+
+	images_array[i].innerHTML = ('<img alt="book.img" onclick=change_location(this) src = '+book_and_author[i+1].img+' >');
+	 
+	document.getElementsByClassName('about'+(i+1))[0].innerHTML = '<i>'+ book_and_author[i+1].bookname+'</i>';
 }
 
 function change_location(arg) {
-	var data_arr = [];
 
+	var data_arr = [];
 	var obj = {};
 	obj.authorname = book_and_author[0].authorname;
+	obj.name = book_and_author[0].authorname;
 	obj.src = arg.getAttribute('src');
 	data_arr.push(obj);
 
     window.localStorage.setItem("book",JSON.stringify(data_arr));
 
     window.location = "../description/description.html";
-
 
 }
