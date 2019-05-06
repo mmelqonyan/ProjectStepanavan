8d10767d16fd51bd33cb333e86115ab97be75730
Gor <gorchshmarityan94@gmail.com>
2019-05-06 23:08:04 +0400

    description

--- pages/description/description.js
+++ pages/description/description.js
@@ -1,18 +1,17 @@
-function processForm() 
-{
+function processForm() {
 
 
-var my = location.search.substring(1).split("&");
-var temp = my[0].split("=");
-l =  decodeURIComponent(temp[1]);
-l = l.replace(/\+/g, " ");
-//l = l.split("+").join(" ")
+    var my = location.search.substring(1).split("&");
+    var temp = my[0].split("=");
+    l = decodeURIComponent(temp[1]);
+    l = l.replace(/\+/g, " ");
+    //l = l.split("+").join(" ")
 
-var js = JSON.parse(l);
+    var js = JSON.parse(l);
 
-console.log(js)
-document.getElementById('book_image').src = js["src"]
-document.getElementById('book_name').innerHTML = js["bookname"]
-document.getElementById('book_author').innerHTML = js["authorname"]
+    console.log(js)
+    document.getElementById('book_image').src = js["src"]
+    document.getElementById('book_name').innerHTML = js["bookname"]
+    document.getElementById('book_author').innerHTML = js["authorname"]
 }
 
