16c7c3088701541bcf9c13431e6aa6facbec810f
gaynane <47750898+gaynane@users.noreply.github.com>
2019-05-09 02:03:36 +0400

    update db

--- database.js
+++ database.js
@@ -1,356 +1,272 @@
-﻿var book_and_author =
+var book_and_author = 
 
 {
 
-
-	"armliter": {
-
-		"tumanyan": {
-			"img": "media/exemp_books/a1.png",
-			"bookname": "Giqor",
-			"authorname": "Hovhannes Tumanyan"
-		},
-		"axayan": {
-			"img": "../../media/exemp_books/a1.png",
-			"bookname": "Giqor",
-			"authorname": "Xazaros Axayan"
-		}
+	
+"armliter":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"}
+	,
+	
+	
+	"axayan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"}
+	
 	},
+	
+	
+	
+"rusLitRus":{
+        
+	"pushkin":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Onegin" , 
+	"authorname":"А.С.Пушкин"},
+	
+	
+	},
+	
+"rusLitArm":{
+        
+	"pushkin":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Եվգենի Օնեգին" , 
+	"authorname":"Ա․Ս․Պուշկին"},
+	
+	},
+	
+"globLitEng":{
+        
+	"shakespeare":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Hamlet" , 
+	"authorname":"William Shakespeare"},
+	
+	
+
+	},	
+
+"globLitFR":{
+        
+	"verne":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Voyage au centre de la Terre " , 
+	"authorname":"Jules Verne"},
+	
+	},
+	
+"globLitRus":{
+        
+	"shakespeare":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Гамлет" , 
+	"authorname":"Уильям Шекспир"},
+	
+	},
+	
+"globLitArm":{
+        
+	"shakespeare":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Համլետ" , 
+	"authorname":"Ուիլյամ Շեքսպիր"},
+		
+	
+
+	},
+
+
+"childbLitAr":{
+        
+	"aper":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Առակներ" , 
+	"authorname":"Խնկո Ապեր"},
+		
+	
+	},
+
+
+"childbLitBan":{
+        
+	"kaputikyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Բանաստեղծություններ" , 
+	"authorname":"Սիլվա Կապուտիկյան"},
+		
+	
+	},
+
+
+"childbLitHeq":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
 
 
 	
+	},
 
+"childbLitPatm":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
+	
+	
+	
+	
+	},
+
+"profLitGeo":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
+	
+	
+	
+	
+	},
+
+
+"profLitCant":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
+	
+	
+	
+	},
+
+
+"profLitBio":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
+	
 	
 
+	
+	},
 
+"profLitPhsi":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
+	
+	
+	
+	},
 
-	"rusLitRus": {
+"profLitPed":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
+	
+	
+	
+	},
 
-		"pushkin": {
-			"img": "media/exemp_books/a1.png",
-			"bookname": "Onegin",
-			"authorname": "А.С.Пушкин"
-		}
+"profLitHis":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
+	
+	
+
+	
 	},
 
 
-
-
-	"rusLitArm": {
-
-		"pushkin": {
-			"img": "../../media/exemp_books/a1.png",
-			"bookname": "Եվգենի Օնեգին",
-			"authorname": "Ա․Ս․Պուշկին"
-		}
+"profLitTech":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
+	
+	
+		
 	},
 
 
-
-	"globLitEng": {
-
-		"shakespeare": {
-			"img": "../../media/exemp_books/a1.png",
-			"bookname": "Hamlet",
-			"authorname": "William Shakespeare"
-		}
+"profLitEcon":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
+	
+	
+	
 	},
 
+"profLitPhil":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
+	
+	
 
-
-
-
-	"globLitFR": {
-
-		"verne": {
-			"img": "../../media/exemp_books/a1.png",
-			"bookname": "Voyage au centre de la Terre ",
-			"authorname": "Jules Verne"
-		}
+	},
+"profLitPhis":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
+	
+	
+	
 	},
 
-
-
-	"globLitRus": {
-
-		"shakespeare": {
-			"img": "../../media/exemp_books/a1.png",
-			"bookname": "Гамлет",
-			"authorname": "Уильям Шекспир"
-		}
-		,
-
-
-
-		"globLitArm": {
-
-			"shakespeare": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Համլետ",
-				"authorname": "Ուիլյամ Շեքսպիր"
-			},
-
-
-
-
-		},
-
-
-		"childbLitAr": {
-
-			"aper": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Առակներ",
-				"authorname": "Խնկո Ապեր"
-			},
-
-
-		},
-
-
-		"childbLitBan": {
-
-			"kaputikyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Բանաստեղծություններ",
-				"authorname": "Սիլվա Կապուտիկյան"
-			},
-
-
-		},
-
-
-		"childbLitHeq": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			},
-
-
-
-		},
-
-		"childbLitPatm": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			},
-
-
-
-
-		},
-
-		"profLitGeo": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-			,
-
-
-
-
-		},
-
-
-		"profLitCant": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-			,
-
-
-
-		},
-
-
-		"profLitBio": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-			,
-
-
-			"axayan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-
-		},
-		"profLitPhsi": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-			,
-
-
-			"axayan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-
-		},
-		"profLitPed": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-			,
-
-
-			"axayan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-
-		},
-		"profLitHis": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-			,
-
-
-			"axayan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-
-		},
-		"profLitTech": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-			,
-
-
-			"axayan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-
-		},
-
-
-		"profLitEcon": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-			,
-
-
-			"axayan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-
-		},
-
-		"profLitPhil": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-			,
-
-
-			"axayan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-
-		},
-		"profLitPhis": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-			,
-
-
-			"axayan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-
-		},
-
-		"LitTranc": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-			,
-
-
-			"axayan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-
-		},
-		"LitEnci": {
-
-			"tumanyan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-			,
-
-
-			"axayan": {
-				"img": "../../media/exemp_books/a1.png",
-				"bookname": "Giqor",
-				"authorname": "Hovhannes Tumanyan"
-			}
-
-		},
-
-
-
-	}
-
-}
+"LitTranc":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
+	
+	
+
+	
+	},
+
+"LitEnc":{
+        
+	"tumanyan":{
+	"img": "../../media/exemp_books/a1.png",
+	"bookname":"Giqor" , 
+	"authorname":"Hovhannes Tumanyan"},
+	
+	
+
+	
+	},
+
+	
+	
+    }
+    
