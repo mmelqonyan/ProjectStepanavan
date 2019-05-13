0623bf50b2e8511aaa97d081aad1a5a328ed141a
HovoMan1 <hovomanasyan@mail.ru>
2019-05-13 22:03:26 +0400

    Add Autors

--- database.js
+++ database.js
@@ -33,14 +33,76 @@
 	
 	},
 	
-"rusLitArm":{
-        
-	"pushkin":{
-	"img": "../../media/exemp_books/a1.png",
-	"bookname":"Եվգենի Օնեգին" , 
-	"authorname":"Ա․Ս․Պուշկին"},
-	
-	},
+{
+"rusLitArm": {
+   	"pushkin": {
+		"shatrvan":{
+  			"img": "media/pushkin_shatrvan.jpg",
+  			"genre":"Պոեմ",
+  			"author": "Ալեքսանդր Սերգեյի Պուշկին",
+ 			"bookname": "Բախչիսարայի Շատրվանը"
+			   },
+		 "dubrovski":{
+  			"img": "media/pushkin_dubrovski.jpg",
+  			"genre":"Վեպ",
+  			"author": "Ալեքսանդր Սերգեյի Պուշկին",
+ 			"bookname": "Դուբրովսկի"
+			     },
+                 "onegin":{
+  			"img": "media/pushkin_onegin.jpg",
+  			"genre":"Չափածո Վեպ",
+  			"author": "Ալեքսանդր Սերգեյի Պուշկին",
+ 			"bookname": "Եվգենի Օնեգին"
+		          },
+                 "godunov":{
+  			"img": "media/pushkin_godunov.jpg",
+  			"genre":"Պիես",
+  			"author": "Ալեքսանդր Սերգեյի Պուշկին",
+ 			"bookname": "Բորիս Գոդունով"
+                           },
+                 "yntir":{
+  			"img": "media/pushkin_yntir.jpg",
+  			"genre":[ "Պոեմ", "Հեքիաթ", "Արձակ"],
+  			"author": "Ալեքսանդր Սերգեյի Պուշկին",
+ 			"bookname": "Ընտիր Երկեր",
+                           },
+                
+                 	
+		},
+        "dostoevskiy": {
+                   "expayrner":{
+                           "img": "media/dostoevski_exbayrner.jpg",
+  			   "genre":"Վեպ",
+  			   "author": "Ֆեոդոր Միխայլովիչ Դոստոևսկի",
+ 			   "bookname": "Կարամազով Եղբայրները"
+                                }
+                              
+
+                       }
+       "bondaryev": {
+                 "yezerq": {
+                           "img": "media/bondaryev_ezerq.jpg",
+  			   "genre":"Վեպ",
+  			   "author": "Յուրի Վասիլևիչ Բոնդարև",
+ 			   "bookname": "Եզերք"  
+
+
+                            }
+
+
+                    }
+       "nikolay_gogol": {
+                       "yntir": {
+  			"img": "media/gogol_yntir.jpg",
+  			"genre":"Վեպ",
+  			"author": "Նիկոլայ Վասիլևիչ Գոգոլ",
+ 			"bookname": "Ընտիր Երկեր",
+                                 }
+                        }
+
+
+}
+
 	
 "globLitEng":{
         
