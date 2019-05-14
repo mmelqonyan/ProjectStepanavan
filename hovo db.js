44b223cabd976e2a12608f2f0cf12bb8b4312a61
HovoMan1 <hovomanasyan@mail.ru>
2019-05-14 19:11:21 +0400

    hovo db

--- hovo db.js
+++ hovo db.js
@@ -1,71 +1,167 @@
-
 {
-    "rusLitArm": {
-        "pushkin": {
-            "shatrvan": {
-                "img": "media/pushkin_shatrvan.jpg",
-                    "genre": "Պոեմ",
-                        "author": "Ալեքսանդր Սերգեյի Պուշկին",
-                            "bookname": "Բախչիսարայի Շատրվանը"
-            }
-            "dubrovski": {
-                "img": "media/pushkin_dubrovski.jpg",
-                    "genre": "Վեպ",
-                        "author": "Ալեքսանդր Սերգեյի Պուշկին",
-                            "bookname": "Դուբրովսկի"
-            },
-            "onegin": {
-                "img": "media/pushkin_onegin.jpg",
-                    "genre": "Չափածո Վեպ",
-                        "author": "Ալեքսանդր Սերգեյի Պուշկին",
-                            "bookname": "Եվգենի Օնեգին"
-            },
-            "godunov": {
-                "img": "media/pushkin_godunov.jpg",
-                    "genre": "Պիես",
-                        "author": "Ալեքսանդր Սերգեյի Պուշկին",
-                            "bookname": "Բորիս Գոդունով"
-            },
-            "yntir": {
-                "img": "media/pushkin_yntir.jpg",
-                    "genre": ["Պոեմ", "Հեքիաթ", "Արձակ"],
-                        "author": "Ալեքսանդր Սերգեյի Պուշկին",
-                            "bookname": "Ընտիր Երկեր"
-            }
-
-
+  "rus": {
+    "hay": {
+      "pushkin": {
+        "author": "Ալեքսանդր Սերգեյի Պուշկին",
+        "shatrvan": {
+          "img": "../media/pushkin_shatrvan.jpg",
+          "genre": "Պոեմ",
+          "authorname": "Ալեքսանդր Սերգեյի Պուշկին",
+          "bookname": "Բախչիսարայի Շատրվանը"
         },
-        "dostoevskiy": {
-            "expayrner": {
-                "img": "media/dostoevski_exbayrner.jpg",
-                    "genre": "Վեպ",
-                        "author": "Ֆեոդոր Միխայլովիչ Դոստոևսկի",
-                            "bookname": "Կարամազով Եղբայրները"
-            }
-
-
+        "dubrovski": {
+          "img": "../media/pushkin_dubrovski.jpg",
+          "genre": "Վեպ",
+          "authorname": "Ալեքսանդր Սերգեյի Պուշկին",
+          "bookname": "Դուբրովսկի"
         },
-        "bondaryev": {
-            "yezerq": {
-                "img": "media/bondaryev_ezerq.jpg",
-                    "genre": "Վեպ",
-                        "author": "Յուրի Վասիլևիչ Բոնդարև",
-                            "bookname": "Եզերք"
-
-
-            }
-
-
+        "onegin": {
+          "img": "../media/pushkin_onegin.jpg",
+          "genre": "Չափածո Վեպ",
+          "authorname": "Ալեքսանդր Սերգեյի Պուշկին",
+          "bookname": "Եվգենի Օնեգին"
         },
-        "nikolay_gogol": {
-            "yntir": {
-                "img": "media/gogol_yntir.jpg",
-                    "genre": "Վեպ",
-                        "author": "Նիկոլայ Վասիլևիչ Գոգոլ",
-                            "bookname": "Ընտիր Երկեր"
-            }
+        "godunov": {
+          "img": "../media/pushkin_godunov.jpg",
+          "genre": "Պիես",
+          "author": "Ալեքսանդր Սերգեյի Պուշկին",
+          "bookname": "Բորիս Գոդունով"
+        },
+        "yntir": {
+          "img": "../media/pushkin_yntir.jpg",
+          "genre": [
+            "Պոեմ",
+            "Հեքիաթ",
+            "Արձակ"
+          ],
+          "authorname": "Ալեքսանդր Սերգեյի Պուշկին",
+          "bookname": "Ընտիր Երկեր"
         }
-
-
+      },
+      "dostoevskiy": {
+        "autor": "Ֆեոդոր Միխայլովիչ Դոստոևսկի",
+        "expayrner": {
+          "img": "../media/dostoevski_exbayrner.jpg",
+          "genre": "Վեպ",
+          "authorname": "Ֆեոդոր Միխայլովիչ Դոստոևսկի",
+          "bookname": "Կարամազով Եղբայրները"
+        }
+      },
+      "bondaryev": {
+        "autor": "Յուրի Վասիլևիչ Բոնդարև",
+        "yezerq": {
+          "img": "../media/bondaryev_ezerq.jpg",
+          "genre": "Վեպ",
+          "authorname": "Յուրի Վասիլևիչ Բոնդարև",
+          "bookname": "Եզերք"
+        }
+      },
+      "nikolay_gogol": {
+        "autor": "Նիկոլայ Վասիլևիչ Գոգոլ",
+        "yntir": {
+          "img": "../media/gogol_yntir.jpg",
+          "genre": "Վեպ",
+          "authorname": "Նիկոլայ Վասիլևիչ Գոգոլ",
+          "bookname": "Ընտիր Երկեր"
+        }
+      }
     }
+  },
+  "mankakan": {
+    "heqiatner": {
+      "tumanyan": {
+        "author": "Հովհաննես Թումանյան",
+        "moxroty": {
+          "img": "../media/tumanyan_moxroty.jpg",
+          "genre": "Հեքիաթ",
+          "authorname": "Հովհաննես Թումանյան",
+          "bookname": "Մոխրոտը"
+        },
+        "chari_verchy": {
+          "img": "../media/tumanyan_chari_verchy.jpg",
+          "genre": "Հեքիաթ",
+          "authorname": "Հովհաննես Թումանյան",
+          "bookname": "Չարի Վերջը"
+        }
+      }
+    },
+    "arakner": {
+      "aygekci": {
+        "author": "Վարդան Այգեկցի",
+        "arakner": {
+          "img": "../media/aygekci_arakner.jpg",
+          "genre": "Առակ",
+          "authorname": "Վարդան Այգեկցի",
+          "bookname": "Առակներ"
+        },
+        "axvesagirq": {
+          "img": "../media/aygekci_axvesagirq.jpg",
+          "genre": "Առակ",
+          "authorname": "Վարդան Այգեկցի",
+          "bookname": "Աղվեսագիրք"
+        }
+      }
+    },
+    "patmvacqner": {
+      "roland": {
+        "author": "Ռոլանդ Շառոյան",
+        "xaxax_kesor": {
+          "img": "../media/roland_xaxax_kesor.jpg",
+          "genre": "Պատմվածքներ",
+          "authorname": "Ռոլանդ Շառոյան",
+          "bookname": "Խաղաղ Կեսօր"
+        },
+        "haryur_krunk": {
+          "img": "../media/roland_haryur_krunk.jpg",
+          "genre": "Պատմվածքներ",
+          "authorname": "Ռոլանդ Շառոյան",
+          "bookname": "Հարյուր Կռունկ"
+        }
+      }
+    },
+    "banastexcutyunner": {
+      "hayrapetyan": {
+        "author": "Հայրապետ Հայրապետյան",
+        "saryaki_tuny": {
+          "img": "../media/hayrapetyan_saryaki_tuny.jpg",
+          "genre": "Բանաստեղծություններ",
+          "authorname": "Հայրապետ Հայրապետյան",
+          "bookname": "Սարյակի Տունը"
+        },
+        "yntir": {
+          "img": "../media/hayrapetyan_yntir.jpg",
+          "genre": "Բանաստեղծություններ",
+          "authorname": "Հայրապետ Հայրապետյան",
+          "bookname": "Ընտիր Երկեր"
+        }
+      }
+    }
+  },
+  "international": {
+    "english": {
+      "conrad": {
+        "author": "Joseph Conrad",
+        "nostromo": {
+          "img": "../media/conrad_nostromo.jpg",
+          "genre": "Նովելներ",
+          "authorname": "Joseph Conrad",
+          "bookname": "Nostromo"
+        },
+        "lord": {
+          "img": "../media/conrad_lord_jim.jpg",
+          "genre": "Նովելներ",
+          "authorname": "Joseph Conrad",
+          "bookname": "Lord Jim"
+        }
+      }
+    }
+  }
 }
+
+
+
+
+
+
+
+
