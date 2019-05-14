808da6f98efa2c9c02aff300bce087230ea7f42c
Ваше Имяvahan024124 <you@example.com>
2019-05-14 21:09:53 +0400

    json

---
+++ vahan.js
@@ -0,0 +1,185 @@
+	{
+"armliter": {
+	"vahangn_davtyan": {
+		        
+             "autho": "Վահագն_Դավթյան",
+                    "amprop":{     
+                        "img":"../media/vahagn-davtyan.jpg",
+			"authoname":"Վահագն_Դավթյան",
+                        "genre": "" ,
+			"bookname":"Ամառային_ամպրոպ"
+	 },
+	}	     
+	"metaqsim": {
+		  "autho":"Մետաքսիմ",
+               		"ser":{
+                         "img":"../media/timthumb.jpeg",
+			 "autho":"Մետաքսիմ",
+                         "genre":"",
+			"bookname":"սերը_հավատ է"
+	},
+
+}
+	"Hovhanes_tumanyan":{
+			"autho":"Հովհաննես_Թումանյան",
+                         "giqor"{
+                        "img":"../media/HQ.jpg",
+			"authoname":"Հովհաննես_Թումանյան",
+                        "genre":"" ,
+			"bookname":"Գիքորը"
+	},
+
+}
+
+	"Hovhanes_Shiraz":{
+		
+                  "autho":"Հովհաննես_Շիրազ",
+                  "garun"{
+		          "img":"../media/Hovhannes_Shiraz.jpeg",
+			  "authoname":"Հովհաննես_Շիրազ",
+		          "genre":"" ,
+			  "bookname":"Գարուն"
+	},
+}
+	"paruyr_sevak":{
+		
+                "autho":"Պարույր_Սևակ",
+               "ankexc":{
+		        "img":"../media/hqdefault.jpg",
+			"authoname":"Պարույր_Սևակ",
+		        "gener":"" ,
+			"bookname":"անկեղծ_ասած "
+	},
+},
+"rusliter":{
+	"lev_tolstoy":{
+		"author":"Лев_Tолстой",
+               "anna":{
+		       "img":"../mediakniga-anna-karenina-.jpg",
+		       "authoname":"Лев_Tолстой",
+		       "genre":"" ,
+		       "bookname":"Анна_Каренина"
+	 },
+	}	     
+		"a.i.kuprin":{
+			
+                           "autho":"А.И.Куприн.",
+                        "izbrannoe":{
+		                "img":"../media",
+				"authoname":"А.И.Куприн.",
+		                "genre":"" ,
+				"bookname":"избранное"
+	},
+
+}
+		"pushkin":{
+			
+                          "author":"Пушкин_А.С",
+                          "dramaticheskie":{  
+		                "img":"../media",
+				"authoname":"Пушкин_А.С",
+		                "genre":"" ,
+				"bookname":"Драматические_произведения"
+	},
+
+
+		"vladmir_korolev":{
+			
+                           "autho":"владимир_короленко",
+                             "muzikant":{
+                        "img":"../media",
+			"autho":"владимир_короленко",
+                        "genre":"" ,
+			"bookname":"Слепой_музыкант"
+	},
+}
+		"maksim_gorki":{
+		
+                       "author":"Максим Горький ",
+		   
+                            "proshay":{                          
+		               "img":"../media",
+			       "authorname":"Максим Горький ",
+		               "genre":"" ,
+			       "bookname":"Прощай!"
+	},
+
+
+},
+"geography"։{
+
+		"vlodavec_v.i":{
+			"author":"Влодавец_В.И.",
+                        "vulkani":{
+		                "img":"../media",
+				"author":"Влодавец_В.И.",
+		                "genre":"" ,
+				"bookname":"Вулканы_Земли"
+	},
+	}
+
+	        "talanov":{
+			"author":"Տալանով",
+                          "nansen":{
+		                 "img":"../media",
+				 "authorname":"Տալանով",
+		                 "genre":"" ,
+				 "bookname":"նանսեն"
+	},
+	
+         }
+"biology"։{
+
+		"sarafyan":{
+			
+                         "author":"Սարաֆյան", 
+                          "kensaritmer":{
+		                "img":"../media",
+				"author":"Սարաֆյան", 
+		                "genre": "" ,
+				"bookname":"կենսառիթմերի_կիթեռնետիկա"
+	},
+	}
+                "dmitreyv":{
+			"author":"Դմիտրիև", 
+		                "kokordilos":{
+		                "img":"../media",
+				"author":"Դմիտրիև", 
+		                "genre":"" ,
+				"bookname":"կոկորդիլոս"
+
+},
+},
+
+
+"psychology"։{
+ 
+          "naxchazyan":{
+                	
+                        "autho":"Նախչազյան",
+		                "hogebanutyun":{
+		                "img":"../media",
+				"author":"Նախչազյան",
+		                "genre": "" ,
+				"bookname":"հոգեբանություն"
+
+},
+}
+         "komarov":{ 
+   	             
+                       "author":"Կոմարով",
+		                "gaxtniq":{
+		                "img":"../media",
+				"authorname":"Կոմարով",
+		                "genre": "" ,
+				"bookname":"գախտնիքի_միյուս_կողմը"
+
+},
+}
+}
+}
+
+
+
+
+
