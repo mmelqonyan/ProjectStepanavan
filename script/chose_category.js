ac2ad4d21f9dad987aadc7a0efe6b191fcddd253
Gor <gorchshmarityan94@gmail.com>
2019-05-19 00:44:24 +0400

    Authentication

--- script/chose_category.js
+++ script/chose_category.js
@@ -10,5 +10,31 @@ function category(el){
 
 }
 
+(function () {
+    const config = {
+        apiKey: "AIzaSyCEKO7yFxF7J2pRZya1oizSvFWrmOal5nk",
+        authDomain: "login-20da7.firebaseapp.com",
+        databaseURL: "https://login-20da7.firebaseio.com",
+        projectId: "login-20da7",
+        storageBucket: "login-20da7.appspot.com",
+        messagingSenderId: "851258903728",
+        appId: "1:851258903728:web:53a25d2a9624f8a8"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(config);
+        
+    const btnLogout = document.getElementById('btnLogout');
 
+    
+    
+
+    btnLogout.addEventListener('click', e => {
+        firebase.auth().signOut();
+        location.replace("../main.html");
+    });
+
+
+    
+
+}());
 
